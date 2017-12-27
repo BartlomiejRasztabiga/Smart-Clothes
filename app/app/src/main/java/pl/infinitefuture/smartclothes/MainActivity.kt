abe634cf8ca4a02ee2202ee259678d57cab05672
Bartłomiej Rasztabiga <bartlomiej.rasztabiga.official@gmail.com>
2017-12-27 20:43:16 +0100

    SC-11 Create read label demo app (#13)

--- app/app/src/main/java/pl/infinitefuture/smartclothes/MainActivity.kt
+++ app/app/src/main/java/pl/infinitefuture/smartclothes/MainActivity.kt
@@ -1,52 +1,25 @@
 package pl.infinitefuture.smartclothes
 
 import android.content.Intent
-import android.nfc.NfcAdapter
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
-import pl.infinitefuture.smartclothes.util.DataFormatter
-import pl.infinitefuture.smartclothes.util.LabelSymbols
-import pl.infinitefuture.smartclothes.util.NFCTools
 
 class MainActivity : AppCompatActivity() {
-
-    private var mNFCAdapter: NfcAdapter? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        mNFCAdapter = NfcAdapter.getDefaultAdapter(this)
+        readLabelButton.setOnClickListener { goToReadLabelActivity() }
     }
 
-    override fun onResume() {
-        super.onResume()
-        mNFCAdapter?.let {
-            NFCTools.enableNFC(it, this, javaClass)
-        }
+    private fun goToReadLabelActivity() {
+        val intent = Intent(this, ReadLabelActivity::class.java)
+        startActivity(intent)
     }
 
-    override fun onPause() {
-        super.onPause()
-        mNFCAdapter?.let {
-            NFCTools.disableNfc(it, this)
-        }
-    }
-
-    override fun onNewIntent(intent: Intent?) {
-        super.onNewIntent(intent)
-
-        val dataOnTag = NFCTools.readFromNFCTag(intent)
-        val labelOnTag = DataFormatter.dataToLabel(dataOnTag)
-        //nfcMessage.text = dataOnTag
-
-        val labelName = labelOnTag.name
-        val labelSymbolsStrings = labelOnTag.symbols
-
-        val labelSymbols = labelSymbolsStrings.map { LabelSymbols.symbols[it] }
-
-        nfcMessage.text = labelName
+    private fun goToWriteLabelActivity() {
 
     }
 }
--- app/app/src/test/java/pl/infinitefuture/smartclothes/util/BasicDataFormatTest.kt
+++ app/app/src/test/java/pl/infinitefuture/smartclothes/util/BasicDataFormatTest.kt
@@ -7,8 +7,8 @@ import pl.infinitefuture.smartclothes.Label
 
 class BasicDataFormatTest {
 
-    private val firstLabel = Label(1, "T-shirt", listOf("WSH-1", "BL-3", "DR-4", "IR-2"))
-    private val firstDataString = "id=1;name=T-shirt;symbols=[WSH-1,BL-3,DR-4,IR-2]"
+    private val firstLabel = Label(1, "Tangle Teezer", listOf("WSH-7", "BL-5", "DR-4", "IR-5", "CH-8", "WE-4"))
+    private val firstDataString = "id=1;name=Tangle Teezer;symbols=[WSH-7,BL-5,DR-4,IR-5,CH-8,WE-4]"
 
     @Test
     fun correctFirstLabelToStringConversion() {
